11de5d5fed7b6df2c342650668c5e8c7c2479483
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-11-25 13:02:20 +0530

    chore: tailwind, prettier, react router setup done; style:Login & sign up page completed

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/sign-in" replace />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
